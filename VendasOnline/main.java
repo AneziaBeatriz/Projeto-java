b34d4f297a0fc6c275449d11d624852b5b80d298
Anezia Beatriz <148148658+AneziaBeatriz@users.noreply.github.com>
2023-12-04 23:15:58 -0300

    Update main.java

--- VendasOnline/main.java
+++ VendasOnline/main.java
@@ -1,94 +1,5 @@
 import java.util.Scanner;
 
-class Produto {
-    private String nome;
-    private double preco;
-    private int quantidadeEmEstoque;
-
-    public Produto(String nome, double preco, int quantidadeEmEstoque) {
-        this.nome = nome;
-        this.preco = preco;
-        this.quantidadeEmEstoque = quantidadeEmEstoque;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public double getPreco() {
-        return preco;
-    }
-
-    public int getQuantidadeEmEstoque() {
-        return quantidadeEmEstoque;
-    }
-
-    public void setQuantidadeEmEstoque(int quantidade) {
-        this.quantidadeEmEstoque = quantidade;
-    }
-
-    public double calcularCustoEnvio() {
-        return 5.0;
-    }
-
-    public void mostrarQuantidadeEstoque() {
-        System.out.println("Quantidade em estoque de " + nome + ": " + quantidadeEmEstoque);
-    }
-
-    public void atualizarQuantidadeEstoque(int novaQuantidade) {
-        quantidadeEmEstoque = novaQuantidade;
-        System.out.println("Quantidade em estoque de " + nome + " atualizada para " + novaQuantidade);
-    }
-}
-
-class Eletronico extends Produto {
-    private String voltagem;
-
-    public Eletronico(String nome, double preco, int quantidadeEmEstoque, String voltagem) {
-        super(nome, preco, quantidadeEmEstoque);
-        this.voltagem = voltagem;
-    }
-
-    @Override
-    public double calcularCustoEnvio() {
-        return super.calcularCustoEnvio() + 2.0;
-    }
-}
-
-class Roupa extends Produto {
-    private String tamanho;
-
-    public Roupa(String nome, double preco, int quantidadeEmEstoque, String tamanho) {
-        super(nome, preco, quantidadeEmEstoque);
-        this.tamanho = tamanho;
-    }
-
-    @Override
-    public double calcularCustoEnvio() {
-        return super.calcularCustoEnvio() + 1.0;
-    }
-}
-
-class Loja {
-    private Produto[] produtos;
-
-    public Loja(int capacidade) {
-        produtos = new Produto[capacidade];
-    }
-
-    public void adicionarProduto(int indice, Produto produto) {
-        if (indice >= 0 && indice < produtos.length) {
-            produtos[indice] = produto;
-        }
-    }
-
-    public void mostrarProdutos() {
-        for (int i = 0; i < produtos.length; i++) {
-            System.out.println((i + 1) + ". " + produtos[i].getNome());
-        }
-    }
-}
-
 public class SistemaVendasOnline {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -96,6 +7,8 @@ public class SistemaVendasOnline {
 
         loja.adicionarProduto(0, new Eletronico("Notebook", 1500.0, 5, "220 V"));
         loja.adicionarProduto(1, new Roupa("Camiseta", 50.0, 10, "M"));
+        loja.adicionarProduto(2, new Eletronico("Smartphone", 800.0, 3, "110 V"));
+        loja.adicionarProduto(3, new Roupa("Calça", 100.0, 20, "M"));
 
         int opcao;
         do {
